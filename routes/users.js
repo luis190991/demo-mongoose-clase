02c8b9ac2317eda37ab96f8169a454cc914049d7
Luis Antonio Ramirez Martinez <luis190991@gmail.com>
2016-11-26 20:47:56 -0700

    Clase final.

--- routes/users.js
+++ routes/users.js
@@ -34,12 +34,45 @@ router.post('/create', function(req, res, next){
   });
 
   user.save(function(err, object){
+    var mensaje = "El elemnto se ha guardado.";
+
     if(err){
       console.log(err);
+    }else{
     }
     res.redirect('/users/');
   });
 
-})
+});
+
+/*
+  Método que se encarga de eliminar elementos de la base de datos.
+*/
+router.post('/remove', function(req, res, next){
+  var id = req.body.id;
+  User.find({"_id":id}).remove(function(err){
+    res.redirect('/users/');
+  });
+});
+
+router.get('/edit/:id', function(req, res, next){
+  User.findOne({"_id":req.params.id}, function(err, user){
+    res.render('users/edit',{user:user});
+  });
+});
+
+router.post('/save', function(req, res, next){
+  User.findOne({"_id":req.body.id}, function(err, user){
+      user.usuario = req.body.usuario;
+      user.nombre = req.body.nombre;
+      user.apellido = req.body.apellido;
+      user.edad = req.body.edad;
+      user.password = req.body.password;
+
+      user.save(function(err){
+        res.redirect('/users/');
+      });
+  });
+});
 
 module.exports = router;
